d660d9a4feb9d1258a94460ea4a5054301f9ebe5
gamarcad <marcadet.gael@gmail.com>
2025-01-14 16:32:56 +0100

    Add TypeScript definitions build to Rollup configuration
    Integrated `rollup-plugin-dts` to generate TypeScript definition files during the build process. Updated `package.json` to include the necessary dependencies and adjusted the `build` script to compile TypeScript first. Modified `.gitignore` to exclude the `types` directory.

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,7 @@
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import json from '@rollup/plugin-json';
+import dts from "rollup-plugin-dts";
 
 export default [
     {
@@ -22,5 +23,11 @@ export default [
             commonjs(), // Convertit CommonJS en ESM
             json(), // Supporte les importations JSON
         ]
-    }
+    },
+    // Build for TypeScript definitions
+    {
+        input: "types/sdk.d.ts", // This path depends on your TypeScript configuration
+        output: [{ file: "dist/index.d.ts", format: "es" }],
+        plugins: [dts()],
+    },
 ];

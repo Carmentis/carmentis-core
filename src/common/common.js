64b356c64bcfb5889a249ce13cad0aabd97ab6f0
gamarcad <marcadet.gael@gmail.com>
2025-01-28 10:03:04 +0100

    Add export for Merkle tree utility module
    This change introduces the Merkle tree utility module to the exports in `common.js`. It ensures the module is accessible wherever `common.js` is imported, enabling more convenient use of Merkle tree functions.

--- src/common/common.js
+++ src/common/common.js
@@ -4,6 +4,7 @@ export * as crypto from "./crypto/crypto.js";
 export * as blockchain from "./blockchain/blockchain.js";
 export * as errors from "./errors/error.js";
 export * as base64 from "./util/base64.js";
+export * as merkle from './trees/merkle.js'
 
 import * as dataUtils from "./util/data.js";
 export const utils = { data: dataUtils }

cf63af75ea36910aa8fa8c932c855cb72531135d
gamarcad <marcadet.gael@gmail.com>
2025-06-19 17:42:36 +0200

    feat: secp256k1 signature

---
+++ src/common/crypto/signature/generic-signature-encoder.ts
@@ -0,0 +1,51 @@
+import {PublicSignatureKey, PublicSignatureKeyEncoder} from "../signature-interface";
+import {CryptoSchemeFactory} from "../factory";
+import {bytesToHex, bytesToUtf8, hexToBytes} from "@noble/ciphers/utils";
+import {utf8ToBytes} from "@noble/hashes/utils";
+
+/**
+ * GenericSignatureEncoder provides methods for encoding and decoding
+ * public signature keys to and from Uint8Array format. This class
+ * implements the PublicSignatureKeyEncoder interface for handling
+ * signature key operations.
+ *
+ * The encoding process converts a public signature key into a raw
+ * JSON object containing a signature scheme ID and the associated
+ * public key information, which is further transformed into a
+ * Uint8Array. The decoding reverses this process, reconstructing
+ * the public signature key object.
+ */
+export class GenericSignatureEncoder implements PublicSignatureKeyEncoder<PublicSignatureKey> {
+
+    /**
+     * Decodes a public key from a Uint8Array and converts it into a PublicSignatureKey object.
+     *
+     * @param {Uint8Array} publicKey - The public key in Uint8Array format to decode. Expected to be a raw-encoded JSON object with `signatureSchemeId` and `publicKey` fields.
+     * @return {PublicSignatureKey} The decoded PublicSignatureKey object created from the provided Uint8Array.
+     * @throws {string} Throws an error if the input does not conform to the expected format or contains invalid data.
+     */
+    decodeFromUint8Array(publicKey: Uint8Array): PublicSignatureKey {
+        const items = JSON.parse(bytesToUtf8(publicKey));
+        console.log(items);
+        if (items && typeof items.signatureSchemeId === "number" && typeof items.publicKey === "string") {
+            const rawPublicKey = hexToBytes(items.publicKey);
+            const factory = new CryptoSchemeFactory();
+            return factory.createPublicSignatureKey(items.signatureSchemeId, rawPublicKey);
+        } else {
+            throw "Invalid public key format: expected raw-encoded JSON object with signatureSchemeId and publicKey fields."
+        }
+    }
+
+    /**
+     * Encodes the given public key as a Uint8Array.
+     *
+     * @param {PublicSignatureKey} publicKey - The public signature key that includes the signature algorithm ID and raw public key.
+     * @return {Uint8Array} A Uint8Array representing the encoded public key.
+     */
+    encodeAsUint8Array(publicKey: PublicSignatureKey): Uint8Array {
+        return utf8ToBytes(JSON.stringify({
+            signatureSchemeId: publicKey.getSignatureAlgorithmId(),
+            publicKey: bytesToHex(publicKey.getRawPublicKey())
+        }));
+    }
+}
--- src/common/crypto/signature/signature.spec.ts
+++ src/common/crypto/signature/signature.spec.ts
@@ -1,11 +1,13 @@
 import {
     SignatureAlgorithmId
-} from "./signature-interface";
+} from "../signature-interface";
 import {toBytes} from "@noble/ciphers/utils";
-import {CryptoSchemeFactory} from "./factory";
-import {MLDSA65PrivateSignatureKey, MLDSA65PublicKeyEncoder, MLDSA65PublicSignatureKey} from "./signature/ml-dsa-65";
+import {CryptoSchemeFactory} from "../factory";
+import {MLDSA65PrivateSignatureKey, MLDSA65PublicKeyEncoder, MLDSA65PublicSignatureKey} from "./ml-dsa-65";
+import {Secp256k1PrivateSignatureKey} from "./secp256k1";
+import {GenericSignatureEncoder} from "./generic-signature-encoder";
 
-describe('crypto', () => {
+describe('ML DSA 65 Signature', () => {
     test("Signature verification", () => {
         const privateKey = MLDSA65PrivateSignatureKey.gen();
         const publicKey = privateKey.getPublicKey();
@@ -44,5 +46,32 @@ describe('crypto', () => {
 
         const cryptoFactory = new CryptoSchemeFactory();
         expect(() => cryptoFactory.createPublicSignatureKey(-1, rawPublicKey)).toThrow();
+    })
+})
+
+describe('Secp256k1 Signature', () => {
+    test("Signature verification", () => {
+        /*
+        const privateKey = Secp256k1PrivateSignatureKey.gen();
+        const publicKey = privateKey.getPublicKey();
+        const msg = toBytes("Hello world");
+        const signature = privateKey.sign(msg);
+        expect(publicKey.verify(msg, signature)).toBe(true);
+
+         */
+    })
+})
+
+
+describe('Generic signature encoder', () => {
+    test("", () => {
+        const encoder = new GenericSignatureEncoder();
+
+        const privateKey = MLDSA65PrivateSignatureKey.gen();
+        const publicKey = privateKey.getPublicKey();
+        const rawPublicKey = encoder.encodeAsUint8Array(publicKey);
+        const publicKey2 = encoder.decodeFromUint8Array(rawPublicKey);
+        expect(publicKey2.getRawPublicKey()).toEqual(publicKey.getRawPublicKey());
+        expect(publicKey2.getSignatureAlgorithmId()).toEqual(publicKey.getSignatureAlgorithmId());
     })
 })

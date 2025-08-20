72728cb7ee6f240fb52a5958d02cf5b4965888f5
Gaël Marcadet <gamarcadet@gmail.com>
2025-08-20 15:00:20 +0200

    dev: addition of CLI to generate key (still having import issue)

---
+++ src/bin/carmentis-cli.ts
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+import {Cli, Command, Option, runExit} from "clipanion";
+import { PrivateSignatureKey } from "../common/crypto/signature/signature-interface";
+import { MLDSA65PrivateSignatureKey } from "../common/crypto/signature/ml-dsa-65";
+import { Secp256k1PrivateSignatureKey } from "../common/crypto/signature/secp256k1";
+
+interface ExportStrategy {
+    export(privateKey: PrivateSignatureKey, filePath: string): void;
+}
+
+class JSONExportStrategy implements ExportStrategy {
+    export(privateKey: PrivateSignatureKey, filePath: string) {
+        // TODO: implémenter la sérialisation et l’écriture fichier
+        console.log(`Exporting private key to ${filePath} in JSON format`);
+    }
+}
+
+function createExportStrategyFromStrategyName(exportStrategyName: string): ExportStrategy {
+    switch (exportStrategyName) {
+        case "json":
+            return new JSONExportStrategy();
+        default:
+            throw new Error(`Export strategy ${exportStrategyName} is not supported.`);
+    }
+}
+
+class GenerateSigKeypairCommand extends Command {
+    static paths = [["generate-sig-keypair"]];
+
+    scheme = Option.String("Scheme", {
+        required: true,
+
+    });
+
+    format = Option.String('Format', {
+        required: true
+    });
+
+    output = Option.String("output", {
+        required: false
+    });
+
+    async execute() {
+        const outputFilename = this.output || "json";
+        const specifiedStrategyName = this.format || 'json';
+        const exportStrategy = createExportStrategyFromStrategyName(specifiedStrategyName);
+
+        if (this.scheme === "mldsa") {
+            const privateKey = MLDSA65PrivateSignatureKey.gen();
+            exportStrategy.export(privateKey, outputFilename);
+        } else if (this.scheme === "secp256k1") {
+            const privateKey = Secp256k1PrivateSignatureKey.gen();
+            exportStrategy.export(privateKey, outputFilename);
+        } else {
+            this.context.stderr.write(`Scheme ${this.scheme} is not supported.\n`);
+        }
+    }
+}
+
+
+const cli = new Cli({
+    binaryLabel: `Carmentis CLI`,
+    binaryName: `carmentis-cli`,
+    binaryVersion: `1.0.0`,
+});
+
+cli.register(GenerateSigKeypairCommand);
+
+cli.runExit(process.argv.slice(2), {
+    ...Cli.defaultContext,
+});

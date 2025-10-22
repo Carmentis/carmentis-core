02a4918d0bcce0624eb5ffc652ecd5268d9b2e18
Gaël Marcadet <gamarcadet@gmail.com>
2025-10-22 17:01:23 +0200

    dev: fix the Utils suite test

--- src/common/utils/Utils.spec.ts
+++ src/common/utils/Utils.spec.ts
@@ -3,6 +3,6 @@ import {Utils} from "./utils";
 describe('TypeManager', () => {
     it('should correctly encode and decode three numbers', () => {
         expect(Utils.binaryFrom(1,0,2)).toBeInstanceOf(Uint8Array);
-        expect(Utils.binaryFrom(1,0, Utils.intToByteArray(2, 6))).toBeInstanceOf(Uint8Array);
+        expect(Utils.binaryFrom(1,0, new Uint8Array(Utils.intToByteArray(2, 6)))).toBeInstanceOf(Uint8Array);
     })
 });

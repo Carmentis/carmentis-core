3a4701340adbe41ac3227452c6032b12257ffd8f
Gaël Marcadet <gamarcadet@gmail.com>
2025-10-22 16:58:09 +0200

    dev: addition of test for Utils

---
+++ src/common/utils/Utils.spec.ts
@@ -0,0 +1,8 @@
+import {Utils} from "./utils";
+
+describe('TypeManager', () => {
+    it('should correctly encode and decode three numbers', () => {
+        expect(Utils.binaryFrom(1,0,2)).toBeInstanceOf(Uint8Array);
+        expect(Utils.binaryFrom(1,0, Utils.intToByteArray(2, 6))).toBeInstanceOf(Uint8Array);
+    })
+});

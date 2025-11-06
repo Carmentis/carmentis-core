afbf2ad1fe0239cf487a04730cbb28ead05bbde3
Gaël Marcadet <gamarcadet@gmail.com>
2025-11-06 10:57:52 +0100

    dev: addition of Networks schema (based on zod 4)

---
+++ src/common/zod/NetworksSchema.ts
@@ -0,0 +1,21 @@
+import { z } from "zod";
+
+export const NetworksFileSchema = z.record(
+    z.string(),
+    z.object({
+        networkName: z.string().optional(),
+        nodes: z.record(
+            z.string(),
+            z.object({
+                nodeName: z.string().optional(),
+                hostname: z.string(),
+                rpcEndpoint: z.string(),
+                p2pEndpoint: z.string(),
+                trusted: z.boolean().optional().default(false),
+                nodeId: z.string(),
+            })
+        ),
+    })
+);
+
+export type NetworksFile = z.infer<typeof NetworksFileSchema>;

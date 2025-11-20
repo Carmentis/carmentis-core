92af00117d3600fe1f952c4dc9c2767c2b9a0d28
Gaël Marcadet <gamarcadet@gmail.com>
2025-11-21 00:15:20 +0100

    dev: add test for VirtualBlockchain microblock appending
    - Introduced a new test for `virtualBlockchain.appendMicroblock` to verify proper microblock addition and account signature handling.
    - Ensured coverage for microblock creation, signing, and retrieval.

---
+++ src/common/blockchain/virtualBlockchains/VirtualBlockchain.spec.ts
@@ -0,0 +1,28 @@
+import {AccountVb} from "./AccountVb";
+import {ProviderFactory} from "../../providers/ProviderFactory";
+import {Microblock} from "../microblock/Microblock";
+import {Secp256k1PrivateSignatureKey, Secp256k1PublicSignatureKey} from "../../crypto/signature/secp256k1";
+import {Utils} from "../../utils/utils";
+
+describe("virtualBlockchain.appendMicroblock", () => {
+    it('should correctly add a microblock', async () => {
+        const provider = ProviderFactory.createInMemoryProvider();
+        const accountVb = new AccountVb(provider);
+        expect(accountVb.isEmpty()).toBeTruthy();
+        expect(accountVb.getHeight()).toEqual(0);
+
+        // create an initial microblock
+        const sk = Secp256k1PrivateSignatureKey.gen();
+        const pk = sk.getPublicKey();
+        const microblock = await AccountVb.createIssuerAccountCreationMicroblock(
+            pk
+        );
+        const signature = microblock.sign(sk);
+        microblock.addAccountSignatureSection({ signature });
+        await accountVb.appendMicroBlock(microblock)
+        expect(accountVb.getHeight()).toEqual(1)
+        expect(accountVb.isEmpty()).toBeFalsy();
+        expect(await accountVb.getPublicKey()).toBeInstanceOf(Secp256k1PublicSignatureKey);
+        expect(await accountVb.getMicroblock(1)).toBeInstanceOf(Microblock)
+    });
+})

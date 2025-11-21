f8eeecf02817a278eefe23f15b935c605e7ab28f
Gaël Marcadet <gamarcadet@gmail.com>
2025-11-21 15:02:17 +0100

    fix: invalid return type

--- src/common/providers/IInternalProvider.ts
+++ src/common/providers/IInternalProvider.ts
@@ -9,7 +9,7 @@ export interface IInternalProvider {
 
     getVirtualBlockchainState(identifier: Uint8Array): Promise<Uint8Array | null>;
 
-    getAccountByPublicKeyHash(publicKeyHash: Uint8Array): Promise<null>;
+    getAccountByPublicKeyHash(publicKeyHash: Uint8Array): Promise<Uint8Array | null>;
 
     setMicroblockVbInformation(identifier: Uint8Array, data: Uint8Array): Promise<void>;
 

e186f98f02937d9b147bc76de2c2155d671560c1
Gaël Marcadet <gamarcadet@gmail.com>
2026-04-02 13:59:33 +0200

    feat: integrate `@noble/hashes` for SHA-512 hashing in SDK
    - Added `sha512` import from `@noble/hashes/sha2`.
    - Updated `@noble/ed25519` to leverage the newly imported `sha512` implementation.

--- src/server/sdk.ts
+++ src/server/sdk.ts
@@ -1,2 +1,6 @@
+import {sha512} from "@noble/hashes/sha2";
+
 export * from "../common/common";
+import {hashes} from "@noble/ed25519";
+hashes.sha512 = sha512;
 export { wiServer } from "./walletInterface/wiServer";
